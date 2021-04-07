928365f4d9d325c4a1cd6130b63a94eae727b155
Alex Basinski <abasinsk@ucsc.edu>
2021-04-07 14:24:51 -0700

    added static starfield background image

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -5,7 +5,14 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
 
+    preload() {
+        this.load.image("starfield", 'assets/starfield.png');
+    }
+
     create() {
+        //display starfield
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, "starfield").setOrigin(0,0);
+
         //green UI background
         this.add.rectangle(0, 
             borderUISize + borderPadding, 
